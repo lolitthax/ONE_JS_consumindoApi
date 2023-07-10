d9d866cfd3c5f5b2155e231d8b034c2f2a4756da
Aline Damião <aline.isilvadamiao@gmail.com>
2023-07-10 14:36:22 -0300

    mensagem de erro

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 async function buscaEndereco(cep) {
+    var msgErro = document.getElementById('erro');
+    msgErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP inválido!');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        msgErro.innerHTML = `<p> CEP inválido. Tente novamente!</p>`;
         console.log(erro);
     }
 }
 let ceps = ['01001000', '01001001'];
 let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
 Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
