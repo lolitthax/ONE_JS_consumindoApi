bc0ab54f6db901761b69f63ec8e235104be2e4f5
Aline Damião <aline.isilvadamiao@gmail.com>
2023-07-10 13:02:59 -0300

    async await - correção nome var

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-async function buscaEncereco() {
+async function buscaEndereco() {
     var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
     var consultaCEPConvertida = await consultaCEP.json();
     console.log(consultaCEPConvertida);
 }
-buscaEncereco();
+buscaEndereco();
